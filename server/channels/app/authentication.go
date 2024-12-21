98c1f91cca51933dbc556837ab72449a22ba62f4
Boris Rybalkin <ribalkin@gmail.com>
2024-12-21 13:46:41 +0100

    logs

--- server/channels/app/authentication.go
+++ server/channels/app/authentication.go
@@ -252,7 +252,7 @@ func checkUserNotBot(user *model.User) *model.AppError {
 
 func (a *App) authenticateUser(rctx request.CTX, user *model.User, password, mfaToken string) (*model.User, *model.AppError) {
 fmt.Println("authenticateUser")
-license := a.Srv().License()
+//license := a.Srv().License()
 	ldapAvailable := *a.Config().LdapSettings.Enable
 
 	if user.AuthService == model.UserAuthServiceLdap {
