d10a45b0abb777e4ea1f1611eb7f710ca4179878
Boris Rybalkin <ribalkin@gmail.com>
2024-12-21 13:30:59 +0100

    logs

--- server/config/database.go
+++ server/config/database.go
@@ -306,7 +306,7 @@ func (ds *DatabaseStore) persist(cfg *model.Config) error {
 
 // Load updates the current configuration from the backing store.
 func (ds *DatabaseStore) Load() ([]byte, error) {
-	fmt.Println("config from db", fs.path)
+	fmt.Println("config from db")
 var configurationData []byte
 
 	row := ds.db.QueryRow("SELECT Value FROM Configurations WHERE Active")
